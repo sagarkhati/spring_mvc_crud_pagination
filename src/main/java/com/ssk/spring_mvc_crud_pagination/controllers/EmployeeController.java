994cced02aad013e399f9ea0e7aba2d1f180f764
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-05-23 10:19:37 +0530

    all employees data is displayed in ui

--- src/main/java/com/ssk/spring_mvc_crud_pagination/controllers/EmployeeController.java
+++ src/main/java/com/ssk/spring_mvc_crud_pagination/controllers/EmployeeController.java
@@ -1,13 +1,25 @@
 package com.ssk.spring_mvc_crud_pagination.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ssk.spring_mvc_crud_pagination.beans.Employee;
+import com.ssk.spring_mvc_crud_pagination.dao.EmployeeDao;
 
 @Controller
 public class EmployeeController {
 
+	@Autowired
+	EmployeeDao empDao;
+	
 	@GetMapping("/viewemp")
-	public String viewEmployees() {
+	public String viewEmployees(Model model) {
+		List<Employee> employees = empDao.getEmployees();
+		model.addAttribute("employees", employees);
 		return "view_emp";
 	}
 	
--- src/main/webapp/WEB-INF/jsp/view_emp.jsp
+++ src/main/webapp/WEB-INF/jsp/view_emp.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>  
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +11,25 @@
 <body>
 <div>
 <h1>Employee List</h1>
+<table border="2" width="70%" cellpadding="2">  
+<tr>
+<th>Id</th>
+<th>Name</th>
+<th>Salary</th>
+<th>Designation</th>
+<th>Edit</th>
+<th>Delete</th>
+</tr>  
+<c:forEach var="emp" items="${employees}">   
+<tr>
+<td>${emp.id}</td>
+<td>${emp.name}</td>
+<td>${emp.salary}</td>
+<td>${emp.designation}</td>
+<td><a href="editemp/${emp.id}">Edit</a></td>
+<td><a href="deleteemp/${emp.id}">Delete</a></td></tr>  
+</c:forEach>  
+</table>
 </div>
 </body>
 </html>
