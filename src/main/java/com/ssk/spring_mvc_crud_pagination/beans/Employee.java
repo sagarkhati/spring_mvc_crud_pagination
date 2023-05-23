f06624abbeb5952f25ea758c6af0a3819e250da8
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-05-23 10:01:58 +0530

    added employee bean

---
+++ src/main/java/com/ssk/spring_mvc_crud_pagination/beans/Employee.java
@@ -0,0 +1,42 @@
+package com.ssk.spring_mvc_crud_pagination.beans;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private int salary;
+	private String designation;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+}
