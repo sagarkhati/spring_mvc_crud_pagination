6d9c068ab56c8d4c14967cb47244ab62e14eaa5a
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-05-23 09:13:13 +0530

    first commit

---
+++ src/main/java/com/ssk/spring_mvc_crud_pagination/SpringMvcCrudPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.ssk.spring_mvc_crud_pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCrudPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCrudPaginationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssk/spring_mvc_crud_pagination/SpringMvcCrudPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssk.spring_mvc_crud_pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcCrudPaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
