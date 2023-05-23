63ea63e60b7a2ae966547775335e4c2fc2878ea6
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-05-23 09:55:22 +0530

    added view emp page and page controller

---
+++ src/main/java/com/ssk/spring_mvc_crud_pagination/controllers/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.ssk.spring_mvc_crud_pagination.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+	@GetMapping("/viewemp")
+	public String viewEmployees() {
+		return "view_emp";
+	}
+}
